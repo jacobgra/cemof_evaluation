394b695e72d4240f2d38ebb5697ea2772d76a7c4
jacobgra <jacobgra@kth.se>
2025-02-20 17:21:42 +0100

    fixed POC word counter that easily counts words in pdfs

---
+++ Code/hawk_counter.py
@@ -0,0 +1,20 @@
+import pdfplumber
+pdf_file = "Data/minutes/penningpolitiskt-protokoll-26-mars-2024.pdf"
+words = ['inflation','ränta ','sysselsättning','pris ','växelkurs','skuld', 'belåning']
+
+# Get text
+text = ''
+with pdfplumber.open(pdf_file) as pdf:
+    for i, page in enumerate(pdf.pages):
+        text = text+'\n'+str(page.extract_text())
+
+# Setup count dictionary
+count = {}
+for elem in words:
+    count[elem] = 0
+        
+# Count occurences
+for i, el in enumerate(words):
+    count[f'{words[i]}'] = text.count(el)
+
+print(count)
