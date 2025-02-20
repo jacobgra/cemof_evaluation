e4aa5af1a881a93015d692b52047c5d2fcbaf7d4
jacobgra <jacobgra@kth.se>
2025-02-20 17:13:24 +0100

    fixed coding error that fetched wrong pdf links

--- Code/pdf_fetch.py
+++ Code/pdf_fetch.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
+from pathlib import Path
 
 
 _base = 'https://www.riksbank.se/sv/press-och-publicerat/dagordningar-och-protokoll/penningpolitiska-protokoll/?year='
-for year in range(2024,2025):
+for year in range(2017,2025):
     _URL = _base + str(year)
     # functional
     r = requests.get(_URL)
@@ -12,15 +13,15 @@ for year in range(2024,2025):
     urls = []
     names = []
     for i, link in enumerate(soup.findAll('a')):
-        _FULLURL = _URL + link.get('href')
+        _FULLURL = link.get('href')
         if _FULLURL.endswith('.pdf'):
-            urls.append(_FULLURL)
+            urls.append("https://www.riksbank.se/"+_FULLURL)
             names.append(soup.select('a')[i].attrs['href'])
 
     names_urls = zip(names, urls)
-
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'}
     for name, url in names_urls:
-        response = requests.get(url)
+        response = requests.get(url, stream = True, headers = headers)
         name = name.split('/')[-1]
         with open('./Data/minutes/'+name, 'wb') as f:
             f.write(response.content)
