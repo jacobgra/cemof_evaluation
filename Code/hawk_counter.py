d71ca9908983fe83b1ad78cef23237b30e384187
jacobgra <jacobgra@kth.se>
2025-02-24 09:20:54 +0100

    saving data from miuntes to csv

--- Code/hawk_counter.py
+++ Code/hawk_counter.py
@@ -3,7 +3,7 @@ import pdfplumber
 import re
 import os
 import pprint
-
+import pandas as pd
 
 def extract_date(filename):
     # Regular expression to match dates like '16 mars 2020'
@@ -14,12 +14,27 @@ def extract_date(filename):
         return f"{match.group(1)} {match.group(2)} {match.group(3)}"
     return None
 
+
+
+def convert_swedish_date(swedish_date):
+    swedish_months = {
+    "januari": "January", "februari": "February", "mars": "March", "april": "April",
+    "maj": "May", "juni": "June", "juli": "July", "augusti": "August",
+    "september": "September", "oktober": "October", "november": "November", "december": "December"
+}
+    for sv, en in swedish_months.items():
+        if sv in swedish_date:
+            swedish_date = swedish_date.replace(sv, en)
+            break
+    return pd.to_datetime(swedish_date, format='%d %B %Y')
+
+
 def extract_word_count(text,words):
     # Setup count dictionary
     governors =  {}
     comments_full = re.search(r'(?s)(Det ekonomiska läget och penningpolitiken)(.*?)§', text).group(2)
     comments_full = comments_full+" §"
-    groups =  re.findall(r'(?s)((?<=Förste vice riksbankschef)|(?<=Vice riksbankschef)|(?<=Riksbankschef))(.*?)((?=Förste vice riksbankschef)|(?=Vice riksbankschef)|(?=Riksbankschef))|((?<=Förste vice riksbankschef)|(?<=Vice riksbankschef)|(?<=Riksbankschef)(.*?)§)', comments_full)
+    groups =  re.findall(r'(?s)((?<=Förste vice riksbankschef )|(?<=Vice riksbankschef )|(?<=Riksbankschef ))(.*?)((?=Förste vice riksbankschef)|(?=Vice riksbankschef)|(?=Riksbankschef))|((?<=Förste vice riksbankschef)|(?<=Vice riksbankschef)|(?<=Riksbankschef)(.*?)§)', comments_full)
     for i in groups:
         """Handle problems with empty first string in groups of match"""
         j = 0
@@ -34,6 +49,8 @@ def extract_word_count(text,words):
         name = list[0]+" "+list[1]
         if (name == "Kerstin af")|(name == "Kerstin af:"):
             name = "Kerstin af Jochnick"
+        if name.endswith(':'):
+            name = name[:-1]
         """If multiple statements, add them together"""
         if name in governors:
             governors[name] = governors[name] + statement
@@ -45,16 +62,18 @@ def extract_word_count(text,words):
             count[elem] = 0
         for i, el in enumerate(words):
             count[f'{words[i]}'] = governors[governor].count(el)
+        count['Total'] = len(governors[governor].split())
         governors[governor] = count
     return governors
 
 def main():
+    data = []
     for entry in os.scandir("Data/minutes/"):
         extracted_date = extract_date(str(entry.path))
         pdf_file = entry.path
         if pdf_file == "Data/minutes/.DS_Store":
             continue
-        print(pdf_file)
+        #print(pdf_file)
         words = ['inflation','ränta ','sysselsättning','pris ','växelkurs','skuld', 'belåning']
         # Get text from PDF source
         text = ''
@@ -63,7 +82,15 @@ def main():
                 text = text+'\n'+str(page.extract_text())
         governors = extract_word_count(text,words)
         print(extracted_date+"\n")
-        pprint.pprint(governors)
+        #pprint.pprint(governors)
+        """Flattening the content of each governor"""
+        for governor, counts in governors.items():
+            row = {'date': extracted_date, 'governor': governor}
+            row.update(counts)
+            data.append(row)
+    df = pd.DataFrame(data)
+    df['date'] = df['date'].apply(convert_swedish_date)
+    df.to_csv('Output/governors_data.csv', index=False)
     return None
     
 if __name__ == "__main__":
